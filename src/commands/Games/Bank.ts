521b5aa181005793ad8637ba857eb29d2e6709eb
TerminatorRsa <100490032+TerminatorRsa@users.noreply.github.com>
2022-02-27 01:48:08 +0200

    Update Bank.ts

--- src/commands/Games/Bank.ts
+++ src/commands/Games/Bank.ts
@@ -8,7 +8,7 @@ import { MessageType, Mimetype } from "@adiwajshing/baileys";
 export default class Command extends BaseCommand {
   constructor(client: WAClient, handler: MessageHandler) {
     super(client, handler, {
-      command: "bank",
+      command: "games",
       description: "Displays user-bank",
       category: "economy",
       usage: `${client.config.prefix}bank`,
